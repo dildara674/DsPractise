310ceac9e25147e17cc5f7380fc470414b413ca8
Dileep Dara <dileep.netdev@gmail.com>
2015-11-07 23:55:49 -0800

    Create ReverseList.cs

---
+++ Dileep.Practise.DataStructures.Problems/LinkedLists/ReverseList.cs
@@ -0,0 +1,68 @@
+public class ReverseList()
+{
+
+public static void TestReverseIterative()
+    {
+        Console.WriteLine("Test started!");
+        SingleLinkedList list = new SingleLinkedList();
+        list.AddFront(3);
+        list.AddFront(2);
+        list.AddFront(1);
+        
+        
+        list.Print();
+        //list = ReverseLinkedListIterative(list);
+        list = ReverseLinkedListRecursive(list);
+        Console.WriteLine("Reverse Ended!");
+        list.Print();
+        Console.WriteLine("Test Ended!");
+    }
+    
+    public static SingleLinkedList ReverseLinkedListRecursive(SingleLinkedList list)
+    {
+        if(list.Head == null) return list;
+        
+        var head = list.Head;
+        var tail = list.Tail;
+        ReverseNodes(list.Head);
+        
+        list.Tail = head;
+        list.Head = tail;
+        
+        return list;
+    }
+    
+    public static void ReverseNodes(LinkedListNode node)
+    {
+        if(node.Next == null)
+        {
+            return;
+        }
+        var next = node.Next;
+        ReverseNodes(next);
+        next.Next = node;
+        node.Next = null;
+    }
+    
+    public static SingleLinkedList ReverseLinkedListIterative(SingleLinkedList list)
+    {
+        Console.WriteLine("Reverse Started!");
+        if(list.Head == null) return null;
+        
+        LinkedListNode current = list.Head;
+        LinkedListNode previous = null;
+        LinkedListNode next = current.Next;
+        
+        while(current != null)
+        {
+            next = current.Next;
+            current.Next = previous;
+            previous = current;
+            current = next;
+        }
+        
+        list.Head = previous;
+        return list;
+    }
+}
+
